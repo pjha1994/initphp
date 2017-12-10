4c69ed1c63e9eca24acdefb60e5af6b27c926738
pjha1994 <pjha1994@rediffmail.com>
2017-12-10 13:48:08 +0000

    init php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+	$heading = 'Welcome'; 
+	$fruit = array('Apple','Orange','Grapes');
+	$keyval = array('fruit1'=>'Apple','fruit2'=>'Orange','fruit3'=>'Grapes');
+	$user = array('first_name'=>'xadnem','last_name'=>'mendax','email'=>'abcd@efgh.ijk');
+	foreach($fruit as $fruits){
+		echo $fruits.'<br>';
+	};
+	<ul>
+	<?php foreach($fruit as $fruits) : ?>
+		<li><?php echo $fruits;?></li>
+	<?php endforeach; ?>
+	</ul>
+?>
+
+<h1>Welcome</h1>
+
+<h2><?php echo $heading;?></h2>
+
+<p><?php echo $fruit[0];?></p>
+
+<p><?php echo $keyval['fruit3'];?></p>
+
+//<p><?php;?></p>

1e8442d6b21d4ab2076471e8c82d933e5db4696b
pjha1994 <pjha1994@rediffmail.com>
2017-12-10 17:28:34 +0000

    initial interface missing file

---
+++ shoutit/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+	<head>	
+		<meta charset='utf-8'/>
+		<title>SHOUT IT</title>
+		<link rel='stylesheet' href='css/style.css' type='text/css'>
+	</head>
+	<body>
+		<div id='container'>
+		<header>
+			<h1>SHOUT IT! SHOUTBOX</h1>
+		</header>	
+			<div id='shouts'>
+				<ul>
+					<li class='shout'><span>10:15PM - </span> Brad : Hey what are you upto?</li>
+					<li class='shout'><span>10:15PM - </span> Brad : Hey what are you upto?</li>
+					<li class='shout'><span>10:15PM - </span> Brad : Hey what are you upto?</li>
+					<li class='shout'><span>10:15PM - </span> Brad : Hey what are you upto?</li>
+					<li class='shout'><span>10:15PM - </span> Brad : Hey what are you upto?</li>
+					<li class='shout'><span>10:15PM - </span> Brad : Hey what are you upto?</li>
+				</ul>
+			</div>
+			<div id='input'>
+				<form method='post' action='process.php'>
+				<input type='text' name='user' placeholder='Enter your name'/>
+				<input type='text' name='message' placeholder='Enter a message'/>
+				<input class='shout-btn' type='submit' name='submit' value-'Shout It Out'/>
+				<br/>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
